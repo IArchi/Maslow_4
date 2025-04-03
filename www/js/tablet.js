222930b8ad5b79fca0de495c385f376855e09cea
md8n <lee@md8n.com>
2025-04-03 09:57:25 +0900

    Bump version number

--- www/js/tablet.js
+++ www/js/tablet.js
@@ -7,7 +7,7 @@ var gCodeDisplayable = false
 var snd = null
 var sndok = true
 
-var versionNumber = "1.02.03";
+var versionNumber = "1.02.04";
 
 const addMessage = (msg, scroll = true, clear = false) => {
   const msgWindow = id("messages");

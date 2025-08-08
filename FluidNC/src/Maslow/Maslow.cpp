7586172dbebeaad518565138550c4a0a3097a709
BarbourSmith <BarbourSmith@users.noreply.github.com>
2025-08-08 08:51:15 -0700

    Update

--- FluidNC/src/Maslow/Maslow.cpp
+++ FluidNC/src/Maslow/Maslow.cpp
@@ -11,7 +11,7 @@
 #include "../Kinematics/MaslowKinematics.h"
 
 // Maslow specific defines
-#define VERSION_NUMBER "1.07"
+#define VERSION_NUMBER "1.08"
 
 #define TLEncoderLine 2
 #define TREncoderLine 1

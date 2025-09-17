0f0ff3fac63f847a2d84c4fef754812d987067ab
BarbourSmith <BarbourSmith@users.noreply.github.com>
2025-09-17 10:44:15 -0700

    Bump version numbers

--- FluidNC/src/Maslow/Maslow.cpp
+++ FluidNC/src/Maslow/Maslow.cpp
@@ -11,7 +11,7 @@
 #include "../Kinematics/MaslowKinematics.h"
 
 // Maslow specific defines
-#define VERSION_NUMBER "1.11"
+#define VERSION_NUMBER "1.12"
 
 #define TLEncoderLine 2
 #define TREncoderLine 1
